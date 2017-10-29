c94ffaa6694b572b556b5672a5320cfb739f9697
Fuxing Loh <lohfuxing@gmail.com>
2017-10-30 01:27:21 +0800

    Added latest ImageParser

--- corpus-place/src/main/java/munch/data/place/parser/ImageParser.java
+++ corpus-place/src/main/java/munch/data/place/parser/ImageParser.java
@@ -1,14 +1,15 @@
 package munch.data.place.parser;
 
+import com.google.common.collect.ImmutableSet;
 import corpus.data.CorpusData;
 import corpus.field.PlaceKey;
-import corpus.images.ImageCachedKey;
+import corpus.images.ImageCachedField;
 import munch.data.structure.Place;
 
 import javax.inject.Singleton;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -20,7 +21,9 @@ import java.util.stream.Collectors;
 @Singleton
 public final class ImageParser extends AbstractParser<List<Place.Image>> {
     private static final int MAX_SIZE = 10;
-    // TODO Image Thumbnail
+    private static final Set<String> ARTICLE_SOURCE_IDS = ImmutableSet.of(
+            "danielfooddiary.com", "sethlui.com", "ladyironchef.com",
+            "misstamchiak.com", "sgfoodonfoot.com", "camemberu.com");
 
     /**
      * @param list list of corpus
@@ -29,15 +32,20 @@ public final class ImageParser extends AbstractParser<List<Place.Image>> {
     @Override
     public List<Place.Image> parse(Place place, List<CorpusData> list) {
         return collect(list, PlaceKey.image).stream()
+                .map(ImageCachedField::new)
+                .filter(field -> field.getImages() != null && field.getSource() != null)
+                .filter(field -> {
+                    if (field.getSource().equals("facebook")) return true;
+                    if (field.getSource().equals("article")) {
+                        return ARTICLE_SOURCE_IDS.contains(field.getSourceId());
+                    }
+                    return false;
+                })
                 .map(field -> {
-                    Map<String, String> images = ImageCachedKey.getImages(field);
-                    String source = ImageCachedKey.getSource(field, null);
-                    if (images == null || source == null) return null;
-
                     Place.Image image = new Place.Image();
-                    image.setWeight(ImageCachedKey.getWeight(field, 1.0));
-                    image.setSource(source);
-                    image.setImages(images);
+                    image.setWeight(field.getWeight(1.0));
+                    image.setSource(field.getSource());
+                    image.setImages(field.getImages());
                     return image;
                 })
                 .filter(Objects::nonNull)
