144fa99ca705e38f378ed36b75d0077089790244
Fuxing <lohfuxing@gmail.com>
2017-12-12 05:03:48 +0800

    New RankingParser will take Facebook and Instagram data into account Article written by the same people will have lower score

--- corpus-place/src/main/java/munch/data/place/parser/RankingParser.java
+++ corpus-place/src/main/java/munch/data/place/parser/RankingParser.java
@@ -1,10 +1,13 @@
 package munch.data.place.parser;
 
 import corpus.data.CorpusData;
+import corpus.field.FieldUtils;
 import munch.data.structure.Place;
 
 import javax.inject.Singleton;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Created by: Fuxing
@@ -19,19 +22,47 @@ public final class RankingParser extends AbstractParser<Double> {
     public Double parse(Place place, List<CorpusData> list) {
         double ranking = 0;
         for (CorpusData data : list) {
-            if (data.getCorpusName().equals("Global.MunchArticle.Article")) {
-                ranking += 10;
-            } else {
-                ranking += 1;
-            }
+            ranking += getCorpusDataScore(data);
         }
+
+        // Article TemplateIds
+        ranking += getArticleScore(list);
+
+        // Image
+        ranking += getImageScore(place);
+        return ranking;
+    }
+
+    private double getCorpusDataScore(CorpusData data) {
+        switch (data.getCorpusName()) {
+            case "Global.Facebook.Place":
+            case "Global.Instagram.Location":
+                return 10;
+            case "Global.MunchArticle.Article":
+                return 2;
+            default:
+                return 1;
+        }
+    }
+
+    private double getArticleScore(List<CorpusData> list) {
+        return list.stream()
+                .filter(data -> data.getCorpusName().equals("Global.MunchArticle.Article"))
+                .map(data -> FieldUtils.get(data, "Article.templateId"))
+                .filter(Optional::isPresent)
+                .map(field -> field.get().getValue())
+                .collect(Collectors.toSet())
+                .size() * 10;
+    }
+
+    private double getImageScore(Place place) {
         if (!place.getImages().isEmpty()) {
             if (place.getImages().size() == 1 && place.getImages().get(0).getSource().equals("munch-image-placeholder")) {
-                ranking += 500;
+                return 500;
             } else {
-                ranking += 1000;
+                return 1000;
             }
         }
-        return ranking;
+        return 0;
     }
 }
