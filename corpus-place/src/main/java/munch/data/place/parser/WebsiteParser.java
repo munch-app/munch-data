0b3db4df3917d963f0d83a50271d4edb2011ddcb
Fuxing <lohfuxing@gmail.com>
2018-02-18 22:11:27 +0800

    Added chope domains to WebsiteParser blockers

--- corpus-place/src/main/java/munch/data/place/parser/WebsiteParser.java
+++ corpus-place/src/main/java/munch/data/place/parser/WebsiteParser.java
@@ -18,7 +18,7 @@ import java.util.regex.Pattern;
 @Singleton
 public final class WebsiteParser extends AbstractParser<String> {
     private static final Pattern HTTP_PATTERN = Pattern.compile("^https?://.*", Pattern.CASE_INSENSITIVE);
-    private static final Set<String> BLOCKED_HOST = Set.of("facebook.com", "instagram.com", "fb.com", "google.com", "burpple.com", "foursquare.com", "hungrygowhere.com", "yelp.com", "zomato.com", "oddle.com", "eatigo.com");
+    private static final Set<String> BLOCKED_HOST = Set.of("facebook.com", "instagram.com", "fb.com", "google.com", "burpple.com", "foursquare.com", "hungrygowhere.com", "yelp.com", "zomato.com", "oddle.com", "eatigo.com", "chope.com", "cho.pe");
 
     @Override
     public String parse(Place place, List<CorpusData> list) {
