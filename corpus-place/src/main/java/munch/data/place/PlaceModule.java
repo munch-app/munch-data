fe6177fc363f02311f2953c0eccddae0d1881075
Fuxing Loh <lohfuxing@gmail.com>
2018-03-09 22:29:32 +0800

    Added missing airtable module for corpus-place

--- corpus-place/src/main/java/munch/data/place/PlaceModule.java
+++ corpus-place/src/main/java/munch/data/place/PlaceModule.java
@@ -1,9 +1,14 @@
 package munch.data.place;
 
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
+import com.amazonaws.services.simplesystemsmanagement.model.GetParameterRequest;
+import com.amazonaws.services.simplesystemsmanagement.model.GetParameterResult;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import corpus.CorpusModule;
+import corpus.airtable.AirtableModule;
 import corpus.data.DataModule;
 import corpus.engine.EngineGroup;
 import munch.data.dynamodb.DynamoModule;
@@ -33,6 +38,17 @@ public class PlaceModule extends AbstractModule {
         install(new munch.data.elastic.ElasticModule());
 
         install(new LocationParserModule());
+
+        install(new AirtableModule(getAirtableKey()));
+    }
+
+    private String getAirtableKey() {
+        AWSSimpleSystemsManagement client = AWSSimpleSystemsManagementClientBuilder.defaultClient();
+        GetParameterResult result = client.getParameter(new GetParameterRequest()
+                .withName("munch-corpus.AirtableKey")
+                .withWithDecryption(true));
+
+        return result.getParameter().getValue();
     }
 
     public static void main(String[] args) throws InterruptedException {
