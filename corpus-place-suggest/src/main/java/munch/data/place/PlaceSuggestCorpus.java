0ab429c7a3d249ee92e12199488d5a0b171e6826
Fuxing <lohfuxing@gmail.com>
2018-03-10 21:01:03 +0800

    PlaceSuggestCorpus validate place id uuid

--- corpus-place-suggest/src/main/java/munch/data/place/PlaceSuggestCorpus.java
+++ corpus-place-suggest/src/main/java/munch/data/place/PlaceSuggestCorpus.java
@@ -11,6 +11,7 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.time.Duration;
 import java.util.Iterator;
+import java.util.regex.Pattern;
 
 /**
  * Created by: Fuxing
@@ -20,6 +21,7 @@ import java.util.Iterator;
  */
 @Singleton
 public final class PlaceSuggestCorpus extends CorpusEngine<CorpusData> {
+    private static final Pattern PATTERN_UUID = Pattern.compile("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$");
     private static final Logger logger = LoggerFactory.getLogger(PlaceSuggestCorpus.class);
 
     private final SuggestMapper suggestMapper;
@@ -39,7 +41,11 @@ public final class PlaceSuggestCorpus extends CorpusEngine<CorpusData> {
     protected Iterator<CorpusData> fetch(long cycleNo) {
         return Iterators.transform(suggestMapper.select(Duration.ofSeconds(5)), input -> {
             CorpusData data = new CorpusData("Sg.MunchUGC.PlaceSuggest", input.getId(), cycleNo);
-            data.setCatalystId(FieldUtils.getValue(data, "Place.id"));
+            String placeId = FieldUtils.getValue(data, "Place.id");
+            if (placeId != null && PATTERN_UUID.matcher(placeId).matches()) {
+                data.setCatalystId(placeId);
+            }
+
             data.setFields(input.getFields());
 
             if (!FieldUtils.has(data, "Suggest.endorsedBy")) return null;
