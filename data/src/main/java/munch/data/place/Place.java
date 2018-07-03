4ccf33ef66e89832a766aa7a23b433c1fc858517
Fuxing Loh <lohfuxing@gmail.com>
2018-07-03 14:12:26 +0800

    Removed email from Place.java, should only exist on Catalyst systems

--- data/src/main/java/munch/data/place/Place.java
+++ data/src/main/java/munch/data/place/Place.java
@@ -32,7 +32,6 @@ public final class Place implements ElasticObject, VersionedObject, SuggestObjec
     private Set<String> names;
     private List<Tag> tags;
 
-    private String email;
     private String phone;
     private String website;
     private String description;
@@ -99,15 +98,6 @@ public final class Place implements ElasticObject, VersionedObject, SuggestObjec
 
     public void setTags(List<Tag> tags) {
         this.tags = tags;
-    }
-
-    @Nullable
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     @Nullable
