cc5aa4d92fead5dfae6ea4f8e913651c5f542ea7
Fuxing Loh <lohfuxing@gmail.com>
2018-07-11 22:20:29 +0800

    Removed deprecated modules

--- corpus-place-tag/tag-model/service/publish.bat
+++
@@ -1,3 +0,0 @@
-docker build -t "munch-data/place-tag-predict" .
-docker tag munch-data/place-tag-predict:latest 197547471367.dkr.ecr.ap-southeast-1.amazonaws.com/munch-data/place-tag-predict:9
-docker push 197547471367.dkr.ecr.ap-southeast-1.amazonaws.com/munch-data/place-tag-predict:9
