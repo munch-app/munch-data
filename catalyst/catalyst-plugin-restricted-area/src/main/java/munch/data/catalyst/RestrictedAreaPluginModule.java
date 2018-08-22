26d9b9397314c9184034c4fb773d6c1157261dcd
Fuxing Loh <lohfuxing@gmail.com>
2018-08-22 23:43:36 +0800

    Fixed RestrictedAreaPluginModule

--- catalyst/catalyst-plugin-restricted-area/src/main/java/munch/data/catalyst/RestrictedAreaPluginModule.java
+++ catalyst/catalyst-plugin-restricted-area/src/main/java/munch/data/catalyst/RestrictedAreaPluginModule.java
@@ -1,6 +1,11 @@
 package munch.data.catalyst;
 
+import catalyst.airtable.AirtableModule;
 import catalyst.plugin.PluginRunner;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagement;
+import com.amazonaws.services.simplesystemsmanagement.AWSSimpleSystemsManagementClientBuilder;
+import com.amazonaws.services.simplesystemsmanagement.model.GetParameterRequest;
+import com.amazonaws.services.simplesystemsmanagement.model.GetParameterResult;
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -12,6 +17,21 @@ import com.google.inject.Injector;
  * Project: munch-data
  */
 public final class RestrictedAreaPluginModule extends AbstractModule {
+    @Override
+    protected void configure() {
+        install(new AirtableModule(getAirtableKey()));
+    }
+
+    private String getAirtableKey() {
+        AWSSimpleSystemsManagement client = AWSSimpleSystemsManagementClientBuilder.defaultClient();
+        GetParameterResult result = client.getParameter(new GetParameterRequest()
+                .withName("munch-corpus.AirtableKey")
+                .withWithDecryption(true));
+
+        return result.getParameter().getValue();
+    }
+
+
     public static void main(String[] args) {
         Injector injector = Guice.createInjector(new RestrictedAreaPluginModule());
         injector.getInstance(PluginRunner.class).run(
