928d9058734e643a887dbc71a391f6cddf51884a
Fuxing Loh <lohfuxing@gmail.com>
2018-12-18 04:50:25 +0800

    Removed Menu images for MenuResolver

--- resolver/src/main/java/munch/data/resolver/MenuResolver.java
+++ resolver/src/main/java/munch/data/resolver/MenuResolver.java
@@ -2,19 +2,16 @@ package munch.data.resolver;
 
 import catalyst.mutation.ImageMutationClient;
 import catalyst.mutation.MutationField;
-import catalyst.mutation.PlaceImageMutation;
 import catalyst.mutation.PlaceMutation;
 import munch.data.place.Place;
 import munch.file.Image;
 import munch.file.ImageClient;
-import munch.restful.core.NextNodeList;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.validation.Valid;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * Created by: Fuxing
@@ -46,11 +43,12 @@ public final class MenuResolver {
     }
 
     private List<Image> getImages(PlaceMutation mutation) {
-        String placeId = mutation.getPlaceId();
-        NextNodeList<PlaceImageMutation> images = imageMutationClient.list(placeId, PlaceImageMutation.Type.menu, null, 4);
-        return images.stream()
-                .map(im -> imageClient.get(im.getImageId()))
-                .collect(Collectors.toList());
+        return List.of();
+//        String placeId = mutation.getPlaceId();
+//        NextNodeList<PlaceImageMutation> images = imageMutationClient.list(placeId, PlaceImageMutation.Type.menu, null, 4);
+//        return images.stream()
+//                .map(im -> imageClient.get(im.getImageId()))
+//                .collect(Collectors.toList());
     }
 
     private String getMenuUrl(PlaceMutation mutation) {
